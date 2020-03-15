a82c92461c07932d76cb49b6a09df3ad3f37d03a
nandara <31009498+nandara@users.noreply.github.com>
2020-03-15 15:11:51 +0000

    Update nandara.php

--- nandara.php
+++ nandara.php
@@ -1,11 +1,11 @@
-<?php
+
 date_default_timezone_set('Asia/Jakarta');
 include "function.php";
 echo color("red","# # # # # # # # # # # # # # # # # # # # # # # \n");
-echo color("yellow","[•]  Time  : ".date('[d-m-Y] [H:i:s]')."   \n");
-echo color("yellow","[•]              waiting proses.....           \n");
-echo color("yellow","[•] cara menulis nomor pakai 62xxxxxxxxxx \n");
-echo color("red","# # # # # # # # Nandara ID # # # # # # # # # # # # # # # \n");
+echo color("blue","[•]  Time  : ".date('[d-m-Y] [H:i:s]')."   \n");
+echo color("blue","[•]              waiting proses.....           \n");
+echo color("blue","[•] cara menulis nomor pakai 62xxxxxxxxxx \n");
+echo color("red","# # # # # # # # # # # Nandara ID # # # # # # # # # # # # \n");
 function change(){
         $nama = nama();
         $email = str_replace(" ", "", $nama) . mt_rand(100, 999);
