07af4b1f6efbfec8e8ad7ad1efb17c10ce73ddcf
nandara <31009498+nandara@users.noreply.github.com>
2020-03-17 12:07:42 +0000

    Update function.php

--- function.php
+++ function.php
@@ -1,15 +1,18 @@
 <?php
+
 function request($url, $token = null, $data = null, $pin = null, $otpsetpin = null, $uuid = null){
+
 $header[] = "Host: api.gojekapi.com";
 $header[] = "User-Agent: okhttp/3.10.0";
 $header[] = "Accept: application/json";
 $header[] = "Accept-Language: id-ID";
 $header[] = "Content-Type: application/json; charset=UTF-8";
-$header[] = "X-AppVersion: 3.46.1";
+$header[] = "X-AppVersion: 3.30.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -6.220".mt_rand(100,999).",106.77".mt_rand(1000,9999);
+$header[] = "X-Location: -6.9212751658159934,107.62244586389556";
+$header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
     endif;
@@ -71,7 +74,6 @@ function getStr1($a,$b,$c,$d){
         $a = @explode($a,$c)[$d];
         return @explode($b,$a)[0];
 }
-
 function color($color = "default" , $text)
     {
         $arrayColor = array(
