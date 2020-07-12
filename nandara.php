de643555fc26f6bf5489a5c5c3f9401f789e6c70
nandara <31009498+nandara@users.noreply.github.com>
2020-07-12 22:20:25 +0700

    Update nandara.php

--- nandara.php
+++ nandara.php
@@ -6,9 +6,9 @@ echo "\e[96m_________________   WARNING !!!!!!!!  _________________ \n";
 echo "\e[96m          INI SCRIB GOJEK INI GRATIS BRO                \n";
 echo "\e[96m           JADI GUNAKANLAH DENGAN BIJAK                 \n";
 echo "\e[96m          SCRIB INI TIDAK DI JUAL BELIKAN               \n";
-echo "\e[96m                             \n";
+echo "\e[96m              SUBCRIBE CHANNEL YOUTUTBE :               \n";
 echo "\e[96m                    NANDARA OFFICIAL                       \n";
-echo "\e[96m                \n";
+echo "\e[96m       BIAR BISA MAKIN SEMANGAT BIKIN KONTENNYA         \n";
 echo "\e[96m ********\e[95m".date('[d-m-Y] [H:i:s]')."\e[96m********\n";
 echo "\e[95m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n";
 echo "\e[96m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n";
@@ -59,7 +59,7 @@ echo "\e[96m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬�
         $uuid = getStr('"resource_owner_id":',',',$verif);
         echo color("nevy","+] Your access token : ".$token."\n\n");
         save("token.txt",$token);
-        echo color("nevy","\n===========(SUROBOYO LOSST REWEL PEGAT)===========");
+        echo color("nevy","\n===========( NANDARA OFFICIAL )===========");
         echo "\n".color("nevy","!] GOFOOD1");
         echo "\n".color("yellow","!] Please wait");
         for($a=1;$a<=3;$a++){
